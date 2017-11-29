46ebbc4b78b52ffb2ce6c7b5843ec6bf761beca9
陆文 <luwen@luwendeMacBook-Pro.local>
2017-11-29 14:33:05 +0800

    init nuxt.js

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  rules: {
+    "no-tabs": 0,
+    "no-multiple-empty-lines": 0,
+    "object-curly-spacing": 0,
+    "no-trailing-spaces": 0,
+    "spaced-comment": 0,
+    "array-bracket-spacing": 0
+  },
+  globals: {}
+}
